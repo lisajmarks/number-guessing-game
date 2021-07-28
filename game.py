e738a4c9634f757ea9421f68adaa3481a4693c82
Lisa Yang <lisajmarks@protonmail.com>
2021-07-28 04:10:03 +0000

    prompt user to guess number

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
 """A number-guessing game."""
 
+#greet player 
 print('hi')
+
+#get player name 
+print('What is your name?')
+print('Enter your name')
+
+name = input('(type in your name)')
+
+print(f"Hello, {name}, I'm thinking of a number between 1 and 100.")
+print('Try to guess my number.')
